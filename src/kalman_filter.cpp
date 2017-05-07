bcdeac0fdd4e720c09289b3932f6c13748f64ce8
robroooh <robroo.pc@gmail.com>
2017-05-08 00:34:16 +0700

    finish the update EKF()

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,5 +1,7 @@
 #include "kalman_filter.h"
+#include <math.h>
 
+#define PI 3.14159265
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
@@ -52,4 +54,41 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+  double px = x_[0];
+  double py = x_[1];
+  float vx = x_[2];
+  float vy = x_[3];
+  VectorXd y(3);
+  VectorXd hx(3);
+
+  float ro = sqrt(pow(px,2) + pow(py,2));
+  float phi = atan2(py,px);
+  float ro_dot = 0.0;
+
+  if(fabs(ro) > 0.0001){
+      ro_dot = ((px*vx) + (py*vy))/ro;
+  }
+
+
+  hx << ro, phi, ro_dot;
+  y = z - hx;
+
+  while (y(1) < -PI){
+    y(1) += 2*PI;
+  }
+  while (y[1] > PI){
+    y(1) -= 2*PI;
+  }
+
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
